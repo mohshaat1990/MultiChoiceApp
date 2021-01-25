9518de93f3e98071600af2311ca2a4a9476fd8a7
sh3at90 <mohamed.sh3t90@gmail.com>
2021-01-25 16:28:08 +0200

    Initial Commit

---
+++ MultiChoiceApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiChoiceApp1
+//
+//  Created by Mohamed Shaat on 1/25/21.
+//  Copyright © 2021 Com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
